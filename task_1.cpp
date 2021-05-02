77b29bb36dd30f209ae563e65a8a472c4c819941
ps@dsys.ru <ps@dsys.ru>
2021-05-03 00:30:58 +0300

    task_2 done

--- task_1.cpp
+++ task_1.cpp
@@ -5,7 +5,6 @@
 int main()
 {
     int user_input;
-
     //infinite cycle
     while(true)
     {
@@ -13,5 +12,14 @@ int main()
         std::cin >> user_input;
         get_day_of_the_week_name(user_input);
     }
+
+#if false
+    std::cout << "do not printing this" << std::endl;
+#endif
+
+#if true
+    std::cout << "printing this" << std::endl;
+#endif
+
     return 0;
 }
